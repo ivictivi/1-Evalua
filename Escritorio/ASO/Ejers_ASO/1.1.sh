0dc47c471ab069cf1b71af1967e511bbd71aa29b
nachiru97 <nachogarcia.mcs@gmail.com>
2016-11-18 08:43:03 +0100

    script1

--- Escritorio/ASO/Ejers_ASO/1.1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Introduce un numero entre el 0 y el 100"
-read var1
-if [ $var1 -ge 0 ] && [ $var1 -le 100 ]
-then echo "$var1 esta entre el 0 y el 100"
-else
-echo "Es superior a 100"
-fi
-
--- Escritorio/ASO/Zenity/2.4factorial_r.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-function factorial (){
-if [ $1 -eq 1 ]
-then echo 1
-else echo `expr $(factorial $(expr $1 - 1)) \* $1`
-fi
-}
-echo "El factorial de $1 es `factorial $1`"
